11dfd7a0d81e3df72f13f2580198a323142cda0b
Your Name <your_email@example.com>
2026-08-05 01:18:20 +0500

    asdf354

--- src/lib/components/DateCard.svelte
+++ src/lib/components/DateCard.svelte
@@ -97,7 +97,7 @@
 						<div class="popover card">
 							<button class="popover-item" onclick={() => moveToToday(todo.id)} disabled={isToday(todo.date)}>
 								<svg class="icon" viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2" /><path d="M3 10h18" /><path d="M8 2v4" /><path d="M16 2v4" /></svg>
-								Move to Today
+								Move Today
 							</button>
 							<button class="popover-delete" onclick={() => removeTodo(todo.id)}>
 								<svg class="icon" viewBox="0 0 24 24"><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" /></svg>
